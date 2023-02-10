1a498c8ce6890e99b66527e3b9a201cc1387f3f0
Abdul Haseeb <mohammedabdulhaseeb4@gmail.com>
2023-02-10 17:49:46 +0530

    fixed issues

--- constant.js
+++
@@ -1,6 +0,0 @@
-let PORT = process.env.PORT;
-let linkHref =`http://localhost:${PORT}/socket.io/socket.io.js`
-let linkElement = document.createElement("link");
-    linkElement.setAttribute("href",linkHref );
-    document.head.appendChild(linkElement);
-export default PORT;
